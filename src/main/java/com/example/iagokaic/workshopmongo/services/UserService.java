e2af575f446ea993116548cd5ce7a5ae3f011f14
Iago Kaic <kaiciago2012@hotmail.com>
2021-07-05 14:48:50 -0400

    Inserção de usuario com POST

--- src/main/java/com/example/iagokaic/workshopmongo/services/UserService.java
+++ src/main/java/com/example/iagokaic/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.iagokaic.workshopmongo.services;
 
 import com.example.iagokaic.workshopmongo.domain.User;
+import com.example.iagokaic.workshopmongo.dto.UserDTO;
 import com.example.iagokaic.workshopmongo.repository.UserRepository;
 import com.example.iagokaic.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
