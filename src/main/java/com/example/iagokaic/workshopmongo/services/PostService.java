7865d8dca259abc1cc4ab0700222fa69fd66af9b
Iago Kaic <kaiciago2012@hotmail.com>
2021-07-05 17:09:26 -0400

    Consulta simples com query methods

--- src/main/java/com/example/iagokaic/workshopmongo/services/PostService.java
+++ src/main/java/com/example/iagokaic/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.example.iagokaic.workshopmongo.services.exception.ObjectNotFoundExcep
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,8 @@ public class PostService {
         return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
+    public List<Post> findByTitle(String text) {
+        return repository.findByTitleContainingIgnoreCase(text);
+    }
 
 }
