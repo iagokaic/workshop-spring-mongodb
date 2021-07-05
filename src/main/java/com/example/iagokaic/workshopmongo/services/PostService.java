c38b71a54a48a595cb4d88fd80624f8edcd51423
Iago Kaic <kaiciago2012@hotmail.com>
2021-07-05 16:36:45 -0400

    Obtendo um post por id

---
+++ src/main/java/com/example/iagokaic/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.example.iagokaic.workshopmongo.services;
+
+import com.example.iagokaic.workshopmongo.domain.Post;
+import com.example.iagokaic.workshopmongo.repository.PostRepository;
+import com.example.iagokaic.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+
+}
